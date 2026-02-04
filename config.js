644d32c21e9b876739faf20dc7189413a6cff704
ladyqwickske <lady.qwickske@gmail.com>
2026-02-04 09:53:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // Set this to your deployed Google Apps Script Web App URL
 // Example: https://script.google.com/macros/s/AKfycb.../exec
-window.GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzTuZqYYiPBykise3AiEeVTx_llL6dkFBT7pxn-3kXx9t_IX2KZx20S1CZ02VxU6QCeqA/exec';
+window.GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzTuZqYYiPBykise3AiEeVTx_llL6dkFBT7pxn-3kXx9t_IX2KZx20S1CZ02VxU6QCeqA/exec?page=management';
+
 
 
 
