f34ddca74cadfb9830c4f6d67b23cea3b82f0672
Lady Q <lady.qwickske@gmail.com>
2026-02-04 10:19:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Set this to your deployed Google Apps Script Web App URL
 // Example: https://script.google.com/macros/s/AKfycb.../exec
-window.GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzUmQuW4NoOjcygFL1KTbF38YTwe_01JynaHKUAiQNZyw9GCJszgEq6UW3y0FrPqyPuZA/exec';
+window.GAS_WEB_APP_URL = 'https://gfw-chests.lady-qwickske.workers.dev/';
+
