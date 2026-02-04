e3dbd846dbd67309b5904a5b1af2c0d8a8a79b60
ladyqwickske <lady.qwickske@gmail.com>
2026-02-04 09:34:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Set this to your deployed Google Apps Script Web App URL
 // Example: https://script.google.com/macros/s/AKfycb.../exec
-window.GAS_WEB_APP_URL = 'https://script.google.com/macros/s/YOUR_WEB_APP_DEPLOYMENT_ID/exec';
+window.GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw48EG5VL80bAJVLgKOJgen9USyxGyeIDcT30ALXT-ZYEj9c562S8FXHqPqNgdHERbL6A/exec';
+
